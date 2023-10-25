2d8b8b5d41b6ce548a4b2e3d9a127b09742d5908
kaizukaaizumi <111713120+kaizukaaizumi@users.noreply.github.com>
2023-10-25 01:12:14 -1000

    added a bunch of quick possible features

--- src/components/LeafletMap/index.tsx
+++ src/components/LeafletMap/index.tsx
@@ -9,12 +9,37 @@ const LeafletMap: React.FC = () => {
         const initializeMap = async () => {
             try {
                 if (mapRef.current) {
-                    const map = L.map(mapRef.current).setView([21.306944, -157.858337], 13);
+                    let map = L.map(mapRef.current).setView([21.306944, -157.858337], 13);
                     L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
                         attribution:
                             'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
                         maxZoom: 18,
                     }).addTo(map);
+                    const marker = L.marker([21.306944, -157.858337]).addTo(map);
+                    const circle = L.circle([21.306944, -157.858337], {
+                        color: 'red',
+                        fillColor: '#f03',
+                        fillOpacity: 0.5,
+                        radius: 500
+                    }).addTo(map);
+                    const polygon = L.polygon([
+                        [21.306944, -157.858337],
+                        [21.4, -157.7],
+                        [21.5, -157.9]
+                    ]).addTo(map);
+                    marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
+                    circle.bindPopup("I am a circle.");
+                    polygon.bindPopup("I am a polygon.");
+                    const popup = L.popup();
+
+                    function onMapClick(e) {
+                        popup
+                            .setLatLng(e.latlng)
+                            .setContent("You clicked the map at " + e.latlng.toString())
+                            .openOn(map);
+                    }
+
+                    map.on('click', onMapClick);
                 }
             } catch (error) {
                 console.log("Error initializing map:", error);
